a13158464e0635dfcf0053af50c3c71ab36a1f67
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-05-07 13:47:04 -0600

    first commit

---
+++ src/test/java/com/william/alumnos/AlumnosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.william.alumnos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlumnosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
