fca8f21767c88500fbb3ec021368f64e7271cdb4
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-05-10 12:35:49 -0600

    actividad5-creando metodo post en el controlardor

--- src/main/java/com/william/alumnos/controllers/AlumnoController.java
+++ src/main/java/com/william/alumnos/controllers/AlumnoController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +19,17 @@ public class AlumnoController {
 
     @Autowired
     private AlumnoRepository alumnoRepository;
-
+    //metodo get para traer todos los alumnos a la base de datos 
     @GetMapping ("/traer-alumnos")
     public List<Alumno> TraerAlumnos() {
         return alumnoRepository.findAll();
+    }
+
+    //metodo para insertar un alumno a la base de datos
+    @PostMapping("/insertar-alumnos")
+    public Alumno insertarAlumno (@RequestBody Alumno alumno){
+        return alumnoRepository.save(alumno);
+
     }
 
 
