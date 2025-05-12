c08c6e22b875b74cc3b2f868b7fee737a8788aa5
williamgl3 <159193524+williamgl3@users.noreply.github.com>
2025-05-11 18:33:44 -0600

    Act18 Creacion de Proyecto Final

--- front_alumnos/src/App.vue
+++ front_alumnos/src/App.vue
@@ -1,57 +1,98 @@
 <script setup>
 import { ref, onMounted} from "vue";
 import axios from "axios";
+import Swal from 'sweetalert2';
 
-const alumnos = ref([]);
+const motocicletas = ref([]);
 
-const nuevoAlumno = ref({
-  nombre: '',
-  apellido:'',
-  carrera:'',
-  telefono:'',
+const nuevaMotocicleta = ref({
+  numeroSerie: '',
+  marca: '',
+  modelo: '',
+  cilindrada: '',
+  color: '',
+  año: '',
+  precio: '',
   imagenURL:''
 });
 
 const editado= ref(false);
 
-const cargarAlumnos = async () => {
-  const response = await axios.get('http://localhost:8080/alumnos/traer-alumnos')
-  alumnos.value = response.data;
-  console.log(alumnos.value);
+const cargarMotocicletas = async () => {
+  const response = await axios.get('http://localhost:8080/motocicletas/traer-motocicletas')
+  motocicletas.value = response.data;
+  console.log(motocicletas.value);
 }
 
-const agregarAlumno = async () =>{
+const agregarMotocicleta = async () =>{
   if (editado.value){
-     await axios.put(`http://localhost:8080/alumnos/editar-alumnos/${nuevoAlumno.value.id}`, nuevoAlumno.value);
+     await axios.put(`http://localhost:8080/motocicletas/editar-motocicletas/${nuevaMotocicleta.value.id}`, nuevaMotocicleta.value);
      editado.value = false
   }else{
-     await axios.post('http://localhost:8080/alumnos/insertar-alumnos', nuevoAlumno.value)
+     await axios.post('http://localhost:8080/motocicletas/insertar-motocicletas', nuevaMotocicleta.value)
+     Swal.fire({
+      icon: 'success',
+      title: 'Motocicleta agregada correctamente',
+      showConfirmButton: false,
+      timer: 1500
+     });
   }
  
-  await cargarAlumnos();
-  nuevoAlumno.value = {
-  nombre: '',
-  apellido:'',
-  carrera:'',
-  telefono:'',
-  imagenURL:''
+  await cargarMotocicletas();
+  nuevaMotocicleta.value = {
+    numeroSerie: '',
+    marca: '',
+    modelo: '',
+    cilindrada: '',
+    color: '',
+    año: '',
+    precio: '',
+    imagenURL:''
   };
 }
 
-const editarAlumnos =(alumno) => {
-  Object.assign(nuevoAlumno.value, alumno);
+const editarMotocicleta =(motocicleta) => {
+  Object.assign(nuevaMotocicleta.value, motocicleta);
   editado.value = true
 }
 
-const eliminarAlumno = async (id) => {
-  await axios.delete(`http://localhost:8080/alumnos/eliminar-alumnos/${id}`)
-  console.log('Alumno eliminado con id', id);
-  await cargarAlumnos();
+const eliminarMotocicleta = async (id) => {
+  Swal.fire({
+    title: '¿Estás seguro de eliminar esta motocicleta?',
+    text: "No podrás revertir esto!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, eliminarla',
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      await eliminarMotocicletaPorId(id);
+      Swal.fire(
+        'Eliminada!',
+        'La motocicleta ha sido eliminada.',
+        'success'
+      )
+    }
+  })
 }
 
-onMounted(cargarAlumnos);
+const eliminarMotocicletaPorId = async (id) => {
+  try{
+    await axios.delete(`http://localhost:8080/motocicletas/eliminar-motocicletas/${id}`)
+    console.log('Motocicleta eliminada con id', id);
+    await cargarMotocicletas();
+  }catch (err) {
+    console.error('Error al eliminar la motocicleta', err);
+    Swal.fire({
+      icon: 'error',
+      title: 'Error al eliminar la motocicleta',
+      text: 'No se pudo eliminar la motocicleta'
+    });
+  }
+}
 
-
+onMounted(cargarMotocicletas);
 </script>
 
 <template>
@@ -59,77 +100,92 @@ onMounted(cargarAlumnos);
     <div class="row">
       <div class="col-md-12 mt-4">
         <div class="card shadow p-4 mb-4">
-          <h2 class="text-center">Formulario de Alumnos</h2>
-          <form @submit.prevent="agregarAlumno">
+          <h2 class="text-center">Formulario de Motocicletas</h2>
+          <form @submit.prevent="agregarMotocicleta">
             <div class="row">
-            <div class="col-md-6 mb-3">
-              <label for="nombre" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="nombre" v-model="nuevoAlumno.nombre" required>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="apellido" class="form-label">Apellidos</label>
-              <input type="text" class="form-control" id="apellido" v-model="nuevoAlumno.apellido" required>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="carrera" class="form-label">Carrera</label>
-              <input type="text" class="form-control" id="carrera" v-model="nuevoAlumno.carrera" required>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="telefono" class="form-label">Telefono</label>
-              <input type="number" class="form-control" id="telefono" v-model="nuevoAlumno.telefono" required>
-            </div>
-            <div class="col-md-6 mb-3">
-              <label for="imagenURL" class="form-label">imagenURL</label>
-              <input type="text" class="form-control" id="imagenURL" v-model="nuevoAlumno.imagenURL">
-            </div>
+              <div class="col-md-6 mb-3">
+                <label for="numeroSerie" class="form-label">Número de Serie</label>
+                <input type="text" class="form-control" id="numeroSerie" v-model="nuevaMotocicleta.numeroSerie" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="marca" class="form-label">Marca</label>
+                <input type="text" class="form-control" id="marca" v-model="nuevaMotocicleta.marca" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="modelo" class="form-label">Modelo</label>
+                <input type="text" class="form-control" id="modelo" v-model="nuevaMotocicleta.modelo" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="cilindrada" class="form-label">Cilindrada (cc)</label>
+                <input type="number" class="form-control" id="cilindrada" v-model="nuevaMotocicleta.cilindrada" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="color" class="form-label">Color</label>
+                <input type="text" class="form-control" id="color" v-model="nuevaMotocicleta.color" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="año" class="form-label">Año</label>
+                <input type="number" class="form-control" id="año" v-model="nuevaMotocicleta.año" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="precio" class="form-label">Precio ($)</label>
+                <input type="number" step="0.01" class="form-control" id="precio" v-model="nuevaMotocicleta.precio" required>
+              </div>
+              <div class="col-md-6 mb-3">
+                <label for="imagenURL" class="form-label">URL de Imagen</label>
+                <input type="text" class="form-control" id="imagenURL" v-model="nuevaMotocicleta.imagenURL">
+              </div>
             </div>
 
             <button type="submit" class="btn btn-primary">
-              {{ editado ? 'Actualizar Alumno' : 'Agregar Alumno' }}
+              {{ editado ? 'Actualizar Motocicleta' : 'Agregar Motocicleta' }}
             </button>
           </form>
-
         </div>
-
       </div>
+      
       <div class="col-md-12">
-            <h2>Tabla de Alumnos</h2>
-             <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Id</th>
-      <th scope="col">Nombre</th>
-      <th scope="col">Apellidos</th>
-      <th scope="col">Carrera</th>
-      <th scope="col">Telefono</th>
-      <th scope="col">Imagen</th>
-      <th scope="col">Acciones</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="alumno in alumnos" :key="alumno.id">
-      <td>{{ alumno.id }}</td>
-      <td>{{ alumno.nombre }}</td>
-      <td>{{ alumno.apellido }}</td>
-      <td>{{ alumno.carrera }}</td>
-      <td>{{ alumno.telefono }}</td>
-      <td><img :src="alumno.imagenURL" alt="Imagen del alumno" width="50"></td>
-      <td>
-        <button @click="eliminarAlumno(alumno.id)" class="btn btn-danger mx-2">
-    <i class="bi bi-trash"></i> Borrar
-  </button>
-        <button @click="editarAlumnos(alumno)" class="btn btn-warning"><i class="bi bi-pencil-fill"></i></button>
-      </td>
-    </tr>
-  </tbody>
-</table>
-</div>
-
-</div>
-       
-   
-</div>
-  
+        <h2>Tabla de Motocicletas</h2>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">ID</th>
+              <th scope="col">N° Serie</th>
+              <th scope="col">Marca</th>
+              <th scope="col">Modelo</th>
+              <th scope="col">Cilindrada</th>
+              <th scope="col">Color</th>
+              <th scope="col">Año</th>
+              <th scope="col">Precio</th>
+              <th scope="col">Imagen</th>
+              <th scope="col">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="motocicleta in motocicletas" :key="motocicleta.id">
+              <td>{{ motocicleta.id }}</td>
+              <td>{{ motocicleta.numeroSerie }}</td>
+              <td>{{ motocicleta.marca }}</td>
+              <td>{{ motocicleta.modelo }}</td>
+              <td>{{ motocicleta.cilindrada }} cc</td>
+              <td>{{ motocicleta.color }}</td>
+              <td>{{ motocicleta.año }}</td>
+              <td>${{ motocicleta.precio }}</td>
+              <td><img :src="motocicleta.imagenURL" alt="Imagen de la motocicleta" width="50"></td>
+              <td>
+                <button @click="eliminarMotocicleta(motocicleta.id)" class="btn btn-danger mx-2">
+                  <i class="bi bi-trash"></i> Borrar
+                </button>
+                <button @click="editarMotocicleta(motocicleta)" class="btn btn-warning">
+                  <i class="bi bi-pencil-fill"></i> Editar
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
